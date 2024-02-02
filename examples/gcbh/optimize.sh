b34391cb17d1f0f3a0f89e605dff2365fe4e6a73
SREE HARSHA BHIMINENI <bsreeharsha@g.ucla.edu>
2024-02-02 11:13:51 -0800

    Removed gcbh.py and provided the link to its GitHub repo

--- examples/gcbh/optimize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=/path/to/scalar_codes
-export VASP_PP_PATH=/path/to/vasp/potentials
-export VASP_COMMAND="mpirun -np ${SLURM_NTASKS} /path/to/vasp/executable"
-
-python geo_opt.py
--- pygcga/__init__.py
+++
@@ -1,4 +0,0 @@
-from .add_adsorbate import add_molecule_on_cluster
-from .add_adsorbate import add_atom_on_surfaces
-from .add_adsorbate import remove_one_adsorbate
-from .mutations import mutation_atoms
